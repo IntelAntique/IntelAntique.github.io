49823dc95b4bcacf790863f18476209b49429848
Han Yu Foong <hanyufoong@gmail.com>
2023-07-14 21:46:47 -0500

    Email working but text styling wonky

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -4,6 +4,18 @@ import emailjs from "@emailjs/browser"
 import React, { useRef } from 'react';
 
 export default function Contact(){
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_1thivhf', 'template_typ9jfx', form.current, 'KVDeSsdK7mZamCtq7')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
     
     return <h1 className='Contact_Me'>
         <h1 className />Find me here!
@@ -18,7 +30,13 @@ export default function Contact(){
         <p>
             Alternatively, you can use this box to message me.
         </p>
-        <h6> Which is a work in progress right now... </h6>
+
+        <form className='Contact_Form' ref={form} onSubmit={sendEmail}>
+            <input className="form-control input-sm" type="text" placeholder='Name' name="from_name" />
+            <input className="form-control input-sm" type="email" placeholder='Email' name="to_name" />
+            <textarea className="form-control input-sm" placeholder='Message' name="message" />
+            <input className="form-control input-sm" type="submit" value="Send" />
+        </form>
         
     </h1>
 }
